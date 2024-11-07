59101f966a709bb2d1aa73d7992797e61858c479
Dmitriy Krivko <dmkrivko@yandex.ru>
2024-11-07 20:44:50 +0300

    add string variable

---
+++ Variables/String.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Variables
+{
+    [CreateAssetMenu]
+    public class StringVariable : ScriptableObject
+    {
+        #if UNITY_EDITOR
+            [Multiline]
+            public string DeveloperDescription = "";
+        #endif
+            public string Value;
+
+            public void SetValue(string value)
+            {
+                Value = value;
+            }
+
+            public void SetValue(StringVariable value)
+            {
+                Value = value.Value;
+            }
+    }
+}
---
+++ Variables/StringReference.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Variables
+{
+    [Serializable]
+    public class StringReference
+    {
+        public bool UseConstant = true;
+        public string ConstantValue;
+        public StringVariable Variable;
+
+        public StringReference()
+        { }
+
+        public StringReference(string value)
+        {
+            UseConstant = true;
+            ConstantValue = value;
+        }
+
+        public string Value
+        {
+            get { return UseConstant ? ConstantValue : Variable.Value; }
+        }
+    }
+}
