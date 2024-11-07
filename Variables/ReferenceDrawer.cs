7817e5a2b75a8861c495c73ac7549e11e400e380
Dmitriy Krivko <dmkrivko@yandex.ru>
2024-11-07 16:19:02 +0300

    add variable reference property drawer

---
+++ Variables/ReferenceDrawer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Variables
+{
+    [CustomPropertyDrawer(typeof(IntReference))]
+    [CustomPropertyDrawer(typeof(FloatReference))]
+    public class ReferenceDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            //base.OnGUI(position, property, label);
+
+            EditorGUI.BeginProperty(position, label, property);
+            EditorGUILayout.BeginVertical("box");
+            var boolRect = new Rect(position.x, position.y, position.width, position.height);
+            var amountRect = new Rect(position.x + 35, position.y, position.width, position.height);
+            var fieldName = property.name;
+            var isConstant = property.FindPropertyRelative("UseConstant").boolValue;
+            var valueType = property.FindPropertyRelative("ConstantValue").type;
+            EditorGUILayout.LabelField($"{fieldName} : {valueType}");
+            EditorGUILayout.BeginHorizontal("box");
+            EditorGUILayout.PropertyField(property.FindPropertyRelative("UseConstant"));
+            if (isConstant)
+            {
+                
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("ConstantValue"), GUIContent.none);
+            }
+            else
+            {
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("Variable"), GUIContent.none);
+            }
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.EndVertical();
+            EditorGUI.EndProperty();
+        }
+    }
+}
